3ad10cee7249515580e3ed03e11dcdbffe882851
Pmaichpm <pmaich.pm@gmail.com>
2024-01-04 17:39:26 -0800

    3-infinite_add.py

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print all the names defined by the compiled module hidden_4."""
+    import hidden_4
+
+    title = dir(hidden_4)
+    for title in names:
+        if title[:2] != "__":
+            print(title)
