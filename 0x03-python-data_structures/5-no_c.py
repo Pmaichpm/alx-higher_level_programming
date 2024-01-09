13c03b9010e00df76cee2ba51003aca2ca0dbe46
Pmaichpm <pmaich.pm@gmail.com>
2024-01-08 16:37:40 -0800

    4-new_in_list.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,11 +4,11 @@ def no_c(my_string):
 
     x = 0
 
-    fresh_string = my_string[:]
+    new_string = my_string[:]
 
     for i in range(l):
         if (my_string[i] == 'c' or my_string[i] == 'C'):
-            fresh_string = fresh_string[:(i - x)] + my_string[(i + 1):]
+            new_string = new_string[:(i - x)] + my_string[(i + 1):]
             x += 1
 
-    return (fresh_string)
+    return (new_string)
