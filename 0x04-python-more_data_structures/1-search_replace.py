e137e58e2a1df4aedb5b5f54de49a45072cf284c
Pmaichpm <pmaich.pm@gmail.com>
2024-01-10 16:49:01 -0800

    0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = my_list[:]
+    for i in range(len(my_list)):
+        if value == search
+        else value for value in my_list
+        return (new_list)
