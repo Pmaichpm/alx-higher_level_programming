5255b3ba2116492bc532f2ce07f15ea414c14816
Pmaichpm <pmaich.pm@gmail.com>
2024-01-10 17:42:42 -0800

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,4 +3,4 @@ def best_score(a_dictionary):
     if (a_dictionary, dict) or len(a_dictionary) == 0:
         return None
 
-    return (max(a_dictionary, key=a_dictionary.get))
+    return (max(a_dictionary, key=a_dictionary.get)
