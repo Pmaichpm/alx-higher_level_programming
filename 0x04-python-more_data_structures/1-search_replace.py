a6eede34db5f060d1afd2ad38e952188f03b1ade
Pmaichpm <pmaich.pm@gmail.com>
2024-01-10 18:15:44 -0800

    12-roman_to_int.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = []
-    for x in my_list:
-    if x == search:
-            new_list.append(replace)
-        else:
-            new_list.append(x)
-        return new_list
+    return [replace if value == search else value for value in my_list]
