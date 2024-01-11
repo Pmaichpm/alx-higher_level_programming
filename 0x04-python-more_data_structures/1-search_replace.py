b10c35cebb86ea2e833dda0fc63673136f7fd12e
Pmaichpm <pmaich.pm@gmail.com>
2024-01-10 16:55:20 -0800

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list[:]
-    for i in range(len(my_list)):
-    if i == search:
+    new_list = []
+    for x in range(len(my_list)):
+    if x == search:
             new_list.append(replace)
         else:
             new_list.append(i)
-        return (new_list)
+        return new_list
